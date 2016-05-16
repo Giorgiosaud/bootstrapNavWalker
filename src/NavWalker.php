092224d2f5da85900b743ed04e9f193a0f963ead
Giorgiosaud <jorgelsaud@gmail.com>
2016-05-16 11:10:10 -0400

    issue with attr not defined

--- src/NavWalker.php
+++ src/NavWalker.php
@@ -3,11 +3,11 @@ namespace jorgelsaud\WordpressTools;
 
 use Walker_Nav_Menu;
 /**
-*
-* Adapted from Edward McIntyre's wp_bootstrap_navwalker class. 
-* Removed support for glyphicon and added support for Font Awesome
-*
-*/
+ *
+ * Adapted from Edward McIntyre's wp_bootstrap_navwalker class. 
+ * Removed support for glyphicon and added support for Font Awesome
+ *
+ */
 /**
  * Class Name: wp_bootstrap_navwalker
  * GitHub URI: https://github.com/twittem/wp-bootstrap-navwalker
@@ -18,7 +18,7 @@ use Walker_Nav_Menu;
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 class NavWalker extends Walker_Nav_Menu {
-	
+
 	/**
 	 * @see Walker::start_lvl()
 	 * @since 3.0.0
@@ -52,93 +52,97 @@ class NavWalker extends Walker_Nav_Menu {
 		 * comparison that is not case sensitive. The strcasecmp() function returns
 		 * a 0 if the strings are equal.
 		 */
-		if ( strcasecmp( $item->attr_title, 'divider' ) == 0 && $depth === 1 ) {
-			$output .= $indent . '<li role="presentation" class="divider">';
-		} else if ( strcasecmp( $item->title, 'divider') == 0 && $depth === 1 ) {
-			$output .= $indent . '<li role="presentation" class="divider">';
-		} else if ( strcasecmp( $item->attr_title, 'dropdown-header') == 0 && $depth === 1 ) {
-			$output .= $indent . '<li role="presentation" class="dropdown-header">' . esc_attr( $item->title );
-		} else if ( strcasecmp($item->attr_title, 'disabled' ) == 0 ) {
-			$output .= $indent . '<li role="presentation" class="disabled"><a href="#">' . esc_attr( $item->title ) . '</a>';
-		} else {
-
-			$class_names = $value = '';
-
-			$classes = empty( $item->classes ) ? array() : (array) $item->classes;
-			$classes[] = 'menu-item-' . $item->ID;
-
-			$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
-
-			/*
-			if ( $args->has_children )
-				$class_names .= ' dropdown';
-			*/
-
-			if($args->has_children && $depth === 0) { $class_names .= ' dropdown'; } elseif($args->has_children && $depth > 0) { $class_names .= ' dropdown-submenu'; }
-
-			if ( in_array( 'current-menu-item', $classes ) )
-				$class_names .= ' active';
-
-			// remove Font Awesome icon from classes array and save the icon
-			// we will add the icon back in via a <span> below so it aligns with
-			// the menu item
-			if ( in_array('fa', $classes)) {
-				$key = array_search('fa', $classes);
-				$icon = $classes[$key + 1];
-				if ( strcasecmp($item->attr_title, 'only-icon' ) != 0 )
-					$icon.=' onlyIcon';
-				$class_names = str_replace($classes[$key+1], '', $class_names);
-				$class_names = str_replace($classes[$key], '', $class_names);
-
-			}
-			
-			$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
-
-			$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
-			$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
-
-			$output .= $indent . '<li' . $id . $value . $class_names .'>';
-
-			$atts = array();
-			$atts['title']  = ! empty( $item->title )	? $item->title	: '';
-			$atts['target'] = ! empty( $item->target )	? $item->target	: '';
-			$atts['rel']    = ! empty( $item->xfn )		? $item->xfn	: '';
-
-			// If item has_children add atts to a.
-			// if ( $args->has_children && $depth === 0 ) {
-			if ( $args->has_children ) {
-				// $atts['href']   		= '';
-				// $atts['href'] = ! empty( $item->url ) ? $item->url : '';
-				$atts['data-toggle']	= 'dropdown';
-				$atts['class']			= 'dropdown-toggle';
-
+		if(isset($item->attr_title)){
+			if ( strcasecmp( $item->attr_title, 'divider' ) == 0 && $depth === 1 ) {
+				$output .= $indent . '<li role="presentation" class="divider">';
+			}else if( strcasecmp( $item->title, 'divider') == 0 ) {
+				$output .= $indent . '<li  class="separator">';
+			}else if ( strcasecmp( $item->title, 'divider') == 0 && $depth === 1 ) {
+				$output .= $indent . '<li role="presentation" class="divider">';
+			} else if ( strcasecmp( $item->attr_title, 'dropdown-header') == 0 && $depth === 1 ) {
+				$output .= $indent . '<li role="presentation" class="dropdown-header">' . esc_attr( $item->title );
+			} else if ( strcasecmp($item->attr_title, 'disabled' ) == 0 ) {
+				$output .= $indent . '<li role="presentation" class="disabled"><a href="#">' . esc_attr( $item->title ) . '</a>';
 			} else {
-				$atts['href'] = ! empty( $item->url ) ? $item->url : '';
-			}
 
-			$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
+				$class_names = $value = '';
 
-			$attributes = '';
-			foreach ( $atts as $attr => $value ) {
-				if ( ! empty( $value ) ) {
-					$value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
-					$attributes .= ' ' . $attr . '="' . $value . '"';
+				$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+				$classes[] = 'menu-item-' . $item->ID;
+
+				$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+
+				/*
+				if ( $args->has_children )
+				$class_names .= ' dropdown';
+				 */
+
+				if($args->has_children && $depth === 0) { $class_names .= ' dropdown'; } elseif($args->has_children && $depth > 0) { $class_names .= ' dropdown-submenu'; }
+
+				if ( in_array( 'current-menu-item', $classes ) )
+					$class_names .= ' active';
+
+				// remove Font Awesome icon from classes array and save the icon
+				// we will add the icon back in via a <span> below so it aligns with
+				// the menu item
+				if ( in_array('fa', $classes)) {
+					$key = array_search('fa', $classes);
+					$icon = $classes[$key + 1];
+					if ( strcasecmp($item->attr_title, 'only-icon' ) == 0 )
+						$icon.=' onlyIcon';
+					$class_names = str_replace($classes[$key+1], '', $class_names);
+					$class_names = str_replace($classes[$key], '', $class_names);
+
 				}
+
+				$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+				$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+				$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+				$output .= $indent . '<li' . $id . $value . $class_names .'>';
+
+				$atts = array();
+				$atts['title']  = ! empty( $item->title )	? $item->title	: '';
+				$atts['target'] = ! empty( $item->target )	? $item->target	: '';
+				$atts['rel']    = ! empty( $item->xfn )		? $item->xfn	: '';
+
+				// If item has_children add atts to a.
+				// if ( $args->has_children && $depth === 0 ) {
+				if ( $args->has_children ) {
+					// $atts['href']   		= '';
+					// $atts['href'] = ! empty( $item->url ) ? $item->url : '';
+					$atts['data-toggle']	= 'dropdown';
+					$atts['class']			= 'dropdown-toggle';
+
+				} else {
+					$atts['href'] = ! empty( $item->url ) ? $item->url : '';
+				}
+
+				$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
+
+				$attributes = '';
+				foreach ( $atts as $attr => $value ) {
+					if ( ! empty( $value ) ) {
+						$value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+						$attributes .= ' ' . $attr . '="' . $value . '"';
+					}
+				}
+
+				$item_output = $args->before;
+
+				// Font Awesome icons
+				if ( ! empty( $icon ) )
+					$item_output .= '<a'. $attributes .'><span class="fa ' . esc_attr( $icon ) . '"></span>&nbsp;';
+				else
+					$item_output .= '<a'. $attributes .'>';	
+				if ( strcasecmp($item->attr_title, 'only-icon' ) != 0 )
+					$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+				$item_output .= ( $args->has_children && 0 === $depth ) ? ' <span class="glyphicon glyphicon-chevron-right"></span></a>' : '</a>';
+				$item_output .= $args->after;
+
+				$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
 			}
-
-			$item_output = $args->before;
-
-			// Font Awesome icons
-			if ( ! empty( $icon ) )
-				$item_output .= '<a'. $attributes .'><span class="fa ' . esc_attr( $icon ) . '"></span>&nbsp;';
-			else
-				$item_output .= '<a'. $attributes .'>';	
-			if ( strcasecmp($item->attr_title, 'only-icon' ) != 0 )
-				$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
-			$item_output .= ( $args->has_children && 0 === $depth ) ? ' <span class="glyphicon glyphicon-chevron-right"></span></a>' : '</a>';
-			$item_output .= $args->after;
-
-			$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
 		}
 	}
 
@@ -163,17 +167,17 @@ class NavWalker extends Walker_Nav_Menu {
 	 * @return null Null on failure with no changes to parameters.
 	 */
 	public function display_element( $element, &$children_elements, $max_depth, $depth, $args, &$output ) {
-        if ( ! $element )
-            return;
+		if ( ! $element )
+			return;
 
-        $id_field = $this->db_fields['id'];
+		$id_field = $this->db_fields['id'];
 
-        // Display this element.
-        if ( is_object( $args[0] ) )
-           $args[0]->has_children = ! empty( $children_elements[ $element->$id_field ] );
+		// Display this element.
+		if ( is_object( $args[0] ) )
+			$args[0]->has_children = ! empty( $children_elements[ $element->$id_field ] );
 
-        parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
-    }
+		parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
+	}
 
 	/**
 	 * Menu Fallback
