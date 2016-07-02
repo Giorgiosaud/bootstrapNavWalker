4b74d3d0f896766e16ab25a3a808be67dc88c73a
Giorgiosaud <jorgelsaud@gmail.com>
2016-07-01 20:23:37 -0400

    fix function

--- functions/functions.php
+++ functions/functions.php
@@ -3,8 +3,8 @@ add_filter('get_custom_logo','change_logo_class');
 
 function change_logo_class($html)
 {
-    $html = str_replace('class="custom-logo"', 'img-fluid', $html);
-    $html = str_replace('class="custom-logo-link"', 'navbar-brand', $html);
+    $html = str_replace('class="custom-logo"', 'class="img-fluid"', $html);
+    $html = str_replace('class="custom-logo-link"', 'class="navbar-brand"', $html);
     return $html;
 }
 ?>
